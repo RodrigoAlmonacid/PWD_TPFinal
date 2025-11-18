7de496c1bbb9e22708a462ba8236c3eb84bc9e3e
aalantorres <116597509+aalantorres@users.noreply.github.com>
2025-11-18 16:30:13 -0300

    UPDATE producto.php
    Incluyo precio y modifico funciones insertar, modificar y eliminar

--- modelo/Producto.php
+++ modelo/Producto.php
@@ -5,18 +5,22 @@ class Producto
     private $idProducto;
     private $nomProducto;
     private $stockProd;
+    private $proPrecio;
     private $mensaje;
     private $detallesProd;
     private $imgProd;
+    private $proDeshabilitado;
 
     public function __construct()
     {
         $this->idProducto = "";
         $this->nomProducto = "";
         $this->stockProd = "";
+        $this->proPrecio = "";
         $this->mensaje = "";
         $this->detallesProd = "";
         $this->imgProd = "";
+        $this->proDeshabilitado = 0;
     }
 
     //Metodos de acceso Get
@@ -35,6 +39,11 @@ class Producto
         return $this->stockProd;
     }
 
+    public function getProPrecio()
+    {
+        return $this->proPrecio;
+    }
+
     public function getMensaje()
     {
         return $this->mensaje;
@@ -45,9 +54,14 @@ class Producto
         return $this->detallesProd;
     }
 
-     public function getImgProd()
+    public function getImgProd()
     {
         return $this->imgProd;
+    }
+
+    public function getProDeshabilitado()
+    {
+        return $this->proDeshabilitado;
     }
 
     //Metodos de acceso Set
@@ -67,6 +81,11 @@ class Producto
         $this->stockProd = $stockProd;
     }
 
+    public function setProPrecio($proPrecio)
+    {
+        $this->proPrecio = $proPrecio;
+    }
+
     public function setMensaje($mensaje)
     {
         $this->mensaje = $mensaje;
@@ -82,26 +101,35 @@ class Producto
         $this->imgProd = $imgProducto;
     }
 
+    public function setProDeshabilitado($proDeshabilitado)
+    {
+        $this->proDeshabilitado = $proDeshabilitado;
+    }
+
     public function __toString()
     {
         return "Producto [ID: " . $this->getIdProducto() .
-           ", Nombre: " . $this->getNomProducto() .
-           ", Stock: " . $this->getStockProducto() .
-           ", Detalles: " . $this->getDetallesProd() .
-           ", Imagen: " . $this->getImgProd() . "]";
+            ", Nombre: " . $this->getNomProducto() .
+            ", Stock: " . $this->getStockProducto() .
+            ", Precio: " . $this->getProPrecio() .
+            ", Detalles: " . $this->getDetallesProd() .
+            ", Imagen: " . $this->getImgProd() .
+            ", Deshabilitado: " . $this->getProDeshabilitado() . "]";
     }
 
     //Metodos
 
     //Cargar Producto
 
-    public function cargar($idProducto, $nomProducto, $detallesProd, $stockProducto, $imgProd)
+    public function cargar($idProducto, $nomProducto, $detallesProd, $stockProducto, $proPrecio, $imgProd, $proDeshabilitado)
     {
         $this->setIdProducto($idProducto);
         $this->setNomProducto($nomProducto);
         $this->setDetallesProd($detallesProd);
         $this->setStockProducto($stockProducto);
+        $this->setProPrecio($proPrecio);
         $this->setImgProd($imgProd);
+        $this->setProDeshabilitado($proDeshabilitado);
     }
 
     //Buscar un producto por id
@@ -119,8 +147,10 @@ class Producto
                     $this->setIdProducto($row['idproducto']);
                     $this->setNomProducto($row['pronombre']);
                     $this->setDetallesProd($row['prodetalle']);
+                    $this->setProPrecio($row['proprecio']);
                     $this->setStockProducto($row['procantstock']);
                     $this->setImgProd($row['proimagen']);
+                    $this->setProDeshabilitado($row['prodeshabilitado']);
                 }
             } else {
                 $this->setMensaje("producto->buscar: " . $base->getError());
@@ -133,45 +163,66 @@ class Producto
 
     //Listar todos los productos
 
-    public function listar()
+    public function listar($parametro = "")
     {
+        $arregloProducto = array();
         $base = new BaseDatos();
-        $consulta = "SELECT * FROM producto;";
-        $arregloProducto = [];
-        if ($base->iniciar()) {
-            if ($base->Ejecutar($consulta)) {
-                $row = $base->Registro();
-                if ($row) {
-                    do {
-                        $objProd = new Producto();
-                        $objProd->setIdProducto($row['idproducto']);
-                        $objProd->setNomProducto($row['pronombre']);
-                        $objProd->setDetallesProd($row['prodetalle']);
-                        $objProd->setStockProducto($row['procantstock']);
-                        $objProd->setImgProd(['proimagen']);
-                        array_push($arregloProducto, $objProd);
-                    } while ($row = $base->Registro());
+        $consulta = "SELECT * FROM producto ";
+
+        if ($parametro != "") {
+            $consulta .= "WHERE " . $parametro;
+        }
+
+        $res = $base->Ejecutar($consulta);
+
+        if ($res > -1) {
+            if ($res > 0) {
+                while ($row = $base->Registro()) {
+                    $objProd = new Producto();
+
+                    $objProd->setIdProducto($row['idproducto']);
+                    $objProd->setNomProducto($row['pronombre']);
+                    $objProd->setDetallesProd($row['prodetalle']);
+                    $objProd->setProPrecio($row['proprecio']);
+                    $objProd->setStockProducto($row['procantstock']);
+                    $objProd->setImgProd($row['proimagen']);
+                    $objProd->setProDeshabilitado($row['prodeshabilitado']);
+
+                    array_push($arregloProducto, $objProd);
                 }
-            } else {
-                $this->setMensaje("producto->listar: " . $base->getError());
             }
         } else {
             $this->setMensaje("producto->listar: " . $base->getError());
         }
+
         return $arregloProducto;
     }
 
     //Insertar un producto
-
     public function insertar()
     {
         $agrega = false;
         $base = new BaseDatos();
-        $consulta = "INSERT INTO producto (pronombre, prodetalle, procantstock, proimagen) VALUES";
-        $consulta .= "('" . $this->getNomProducto() . "', '" . $this->getDetallesProd() . "', '" . $this->getStockProducto() . "', '" . $this->getImgProd() . "');";
+        $deshabilitado = $this->getProDeshabilitado();
+        if ($deshabilitado === null || $deshabilitado === "") {
+            $valDeshabilitado = 0;
+        } else {
+            $valDeshabilitado = $deshabilitado;
+        }
+
+        $consulta = "INSERT INTO producto (pronombre, prodetalle, procantstock, proimagen, proprecio, prodeshabilitado) VALUES";
+        $consulta .= "('" . $this->getNomProducto() . "', ";
+        $consulta .= "'" . $this->getDetallesProd() . "', ";
+        $consulta .= $this->getStockProducto() . ", ";
+        $consulta .= "'" . $this->getImgProd() . "', ";
+        $consulta .= $this->getProPrecio() . ", ";
+        $consulta .= $valDeshabilitado . ");";
+
         if ($base->iniciar()) {
-            if ($base->Ejecutar($consulta)) {
+            $idGenerado = $base->Ejecutar($consulta);
+            if ($idGenerado > 0) {
                 $agrega = true;
+                $this->setIdProducto($idGenerado);
             } else {
                 $this->setMensaje("producto->insertar: " . $base->getError());
             }
@@ -189,8 +240,9 @@ class Producto
         $base = new BaseDatos();
         $modifica = false;
         $consulta = "UPDATE producto SET ";
-        $consulta .= "pronombre='" . $this->getNomProducto() . "', prodetalle='" . $this->getDetallesProd() . "', procantstock='" . $this->getStockProducto() . "', proimagen='" . $this->getImgProd();
-        $consulta .= "' WHERE idproducto=" . $this->getIdProducto() . ";";
+        $consulta .= "pronombre='" . $this->getNomProducto() . "', prodetalle='" . $this->getDetallesProd() . "', procantstock=" . $this->getStockProducto() . ", proimagen='" . $this->getImgProd();
+        $consulta .= "', proprecio=" . $this->getProPrecio() . ", prodeshabilitado=" . $this->getProDeshabilitado() . " WHERE idproducto=" . $this->getIdProducto() . ";";
+        $consulta .= "', proprecio=" . $this->getProPrecio() . ", prodeshabilitado=" . $this->getProDeshabilitado() . " WHERE idproducto=" . $this->getIdProducto() . ";";
         if ($base->iniciar()) {
             if ($base->Ejecutar($consulta)) {
                 $modifica = true;
@@ -205,19 +257,20 @@ class Producto
 
     //eliminar un producto
 
-    public function eliminar(){
-        $base=new BaseDatos();
-        $elimina=false;
-        $consulta="DELETE FROM producto WHERE idproducto=".$this->getIdProducto().";";
-        if($base->iniciar()){
-            if($base->Ejecutar($consulta)){
-                $elimina=true;
-            }
-            else {
+    public function eliminar()
+    {
+        $base = new BaseDatos();
+        $elimina = false;
+        $consulta = "UPDATE producto SET prodeshabilitado = 1 WHERE idproducto=" . $this->getIdProducto() . ";";
+
+        if ($base->iniciar()) {
+            if ($base->Ejecutar($consulta)) {
+                $elimina = true;
+                $this->setProDeshabilitado(1);
+            } else {
                 $this->setMensaje("producto->eliminar: " . $base->getError());
-            } 	
-        }
-        else {
+            }
+        } else {
             $this->setMensaje("producto->eliminar: " . $base->getError());
         }
         return $elimina;
