2c7a6d9756ed52bae41dcdaca664f130ed410d20
RodrigoAlmonacid <rodrigo.almonacid@est.fi.uncoma.edu.ar>
2026-01-22 14:48:34 -0300

    subir archivos

--- vista/accion/accionProducto.php
+++ vista/accion/accionProducto.php
@@ -8,14 +8,15 @@ $datos = getSubmittedData(); //la operación viene por get, los demás datos por
 $abmProducto = new ABMProducto();
 $respuesta = array('success' => false, 'errorMsg' => 'Operación no reconocida.');
 
-/*
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+
+$target_dir = "../imagenes/";
+$target_file = $target_dir . basename($datos["imgProducto"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$datos['proimagen']=$target_file;
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+if(isset($datos["submit"])) {
+  $check = getimagesize($datos["imgProducto"]["tmp_name"]);
   if($check !== false) {
     echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
@@ -24,7 +25,38 @@ if(isset($_POST["submit"])) {
     $uploadOk = 0;
   }
 }
-  documentacion:
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($datos["imgProducto"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($datos["imgProducto"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $datos["imgProducto"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+/*
+documentacion:
 $target_dir = "uploads/" - especifica el directorio donde se colocará el archivo
 $target_file especifica la ruta del archivo que se cargará
 $uploadOk=1 aún no se utiliza (se utilizará más adelante)
